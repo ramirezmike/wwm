2539256b9eeba8655d166c0c140883c370922e4b
mramirez <ramirezmike2@gmail.com>
2020-12-16 18:28:15 -0500

    WIP

--- src/renderer/win.rs
+++ src/renderer/win.rs
@@ -27,12 +27,25 @@ impl Renderer for WinRenderer {
         let mut top = y;
         let mut bottom = y + height;
 
+        let use_dpi: bool;
+
         unsafe {
-            let border_width = GetSystemMetricsForDpi(SM_CXFRAME, display.dpi);
-            let border_height = GetSystemMetricsForDpi(SM_CYFRAME, display.dpi);
+            let border_width = match GetSystemMetricsForDpi(SM_CXFRAME, display.dpi) {
+                                  0 => {
+                                      use_dpi = false;
+                                      GetSystemMetrics(SM_CXFRAME)
+                                  },
+                                  x @ _ => {
+                                      use_dpi = true;
+                                      x
+                                  }
+                               };
+            let border_height = if use_dpi { GetSystemMetricsForDpi(SM_CYFRAME, display.dpi) } 
+                                else { GetSystemMetrics(SM_CYFRAME) };
 
             if rule.chromium || rule.firefox || !config.remove_title_bar {
-                let caption_height = GetSystemMetricsForDpi(SM_CYCAPTION, display.dpi);
+                let caption_height = if use_dpi { GetSystemMetricsForDpi(SM_CYCAPTION, display.dpi) }
+                                     else { GetSystemMetrics(SM_CYCAPTION) };
                 top += caption_height;
             } else {
                 top -= border_height * 2;
@@ -49,20 +62,30 @@ impl Renderer for WinRenderer {
                 || rule.chromium
                 || (!config.remove_title_bar && rule.has_custom_titlebar)
             {
-                if rule.firefox {
-                    left -= (border_width as f32 * 1.5) as i32;
-                    right += (border_width as f32 * 1.5) as i32;
-                    bottom += (border_height as f32 * 1.5) as i32;
-                } else if rule.chromium {
-                    top -= border_height / 2;
-                    left -= border_width * 2;
-                    right += border_width * 2;
-                    bottom += border_height * 2;
+                if rule.firefox || rule.chromium {
+                    let mut clientRect = RECT { bottom: 0, left: 0, right: 0, top: 0 };
+                    GetClientRect(window.id.into(), &mut clientRect);
+                    let mut windowRect = RECT { bottom: 0, left: 0, right: 0, top: 0 };
+                    GetWindowRect(window.id.into(), &mut windowRect);
+
+                    if rule.firefox {
+                        top += (((windowRect.bottom - windowRect.top) - clientRect.bottom) as f32 * 1.0) as i32;
+                        left += ((windowRect.right - windowRect.left) - clientRect.right);
+                        right -= ((windowRect.right - windowRect.left) - clientRect.right);
+                    } else if rule.chromium {
+                        top += (windowRect.bottom - windowRect.top) - clientRect.bottom;
+                    }
+                    /*
+                    left -= ((windowRect.right - windowRect.left) - clientRect.right) / 4;
+                    right += ((windowRect.right - windowRect.left) - clientRect.right) / 4;
+                    bottom += ((windowRect.bottom - windowRect.top) - clientRect.bottom) / 4;
+                    */
+                } else {
+                    left += border_width * 2;
+                    right -= border_width * 2;
+                    top += border_height * 2;
+                    bottom -= border_height * 2;
                 }
-                left += border_width * 2;
-                right -= border_width * 2;
-                top += border_height * 2;
-                bottom -= border_height * 2;
             } else {
                 top += border_height * 2;
             }
@@ -78,12 +101,22 @@ impl Renderer for WinRenderer {
         // println!("before {}", rect_to_string(rect));
 
         unsafe {
-            AdjustWindowRectEx(
-                &mut rect,
-                window.style.bits() as u32,
-                0,
-                window.exstyle.bits() as u32,
-            );
+            if use_dpi {
+                AdjustWindowRectExForDpi(
+                    &mut rect,
+                    window.style.bits() as u32,
+                    0,
+                    window.exstyle.bits() as u32,
+                    display.dpi
+                );
+            } else {
+                AdjustWindowRectEx(
+                    &mut rect,
+                    window.style.bits() as u32,
+                    0,
+                    window.exstyle.bits() as u32,
+                );
+            }
         }
 
         // println!("after {}", rect_to_string(rect));
